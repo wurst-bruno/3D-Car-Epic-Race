565b039f001be11b64e5457964e41efbe0dfa00c
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-05-27 14:42:33 -0300

    Fin clase 27/5

---
+++ Assets/B-Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float RotationSpeed;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            //transform.position += new Vector3(0, 0, movementSpeed); es lo mismo que lo de acá abajo
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, RotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -RotationSpeed, 0);
+        }
+
+
+    }
+}
