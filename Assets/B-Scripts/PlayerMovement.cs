4ffbcee9491e8cf2a75fff3eb915c7dcc8d04ef2
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-05-31 15:13:57 -0300

    Mecánicas de Velocidad
    Estas son las mecánicas para que cuando toque el pasto, baje la velocidad del Player.

--- Assets/B-Scripts/PlayerMovement.cs
+++ Assets/B-Scripts/PlayerMovement.cs
@@ -6,35 +6,53 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float movementSpeed;
+    float movementSpeedBackUp;
     public float RotationSpeed;
 
    
     void Start()
     {
-
+        movementSpeedBackUp = movementSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow))
         {
             //transform.position += new Vector3(0, 0, movementSpeed); es lo mismo que lo de acá abajo
             transform.Translate(0, 0, movementSpeed);
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(0, 0, -movementSpeed);
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(0, RotationSpeed, 0);
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(0, -RotationSpeed, 0);
         }
 
 
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Pasto")
+        {
+            movementSpeed = movementSpeed/2;
+
+        }
+        
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.name == "Pasto")
+        {
+            movementSpeed = movementSpeedBackUp;
+        }
     }
 }
