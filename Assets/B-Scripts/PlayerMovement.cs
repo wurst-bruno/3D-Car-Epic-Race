e87fcfc2ada57fcd7368ee9ed6f1d4dc3dd70f6e
wurst-bruno <bruno.wurstt@gmail.com>
2022-06-06 18:30:15 -0300

    Fin 6/6 | Casa

--- Assets/B-Scripts/PlayerMovement.cs
+++ Assets/B-Scripts/PlayerMovement.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Text vel_actual;
 
     public float movementSpeed;
     float movementSpeedBackUp;
     public float RotationSpeed;
-
-
 
 
     void Start()
@@ -22,26 +23,28 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        transform.Translate(0, 0, movementSpeed * Input.GetAxis("Vertical"));
-        transform.Rotate(0, RotationSpeed, 0 * Input.GetAxis("Horizontal"));
 
-        //if (Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow))
+        transform.Translate(0, 0, movementSpeed * Input.GetAxis("Vertical"));
+
+        //if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         //{
         //    //transform.position += new Vector3(0, 0, movementSpeed); es lo mismo que lo de acá abajo
         //    transform.Translate(0, 0, movementSpeed);
-        ////            }
+        //}
         //if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         //{
         //    transform.Translate(0, 0, -movementSpeed);
         //}
-        //if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.Rotate(0, RotationSpeed, 0);
-        //}
-        //if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.Rotate(0, -RotationSpeed, 0);
-        //}
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, RotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, -RotationSpeed, 0);
+        }
+
+        vel_actual.text = "Velocidad actual:  " + movementSpeed.ToString();
 
     }
 
@@ -63,11 +66,11 @@ public class PlayerMovement : MonoBehaviour
     {
         if (collisionInfo.gameObject.tag == "Asfalto")
         {
-            movementSpeed = 0.1f;
+            movementSpeed = 0.4f;
         }
         if (collisionInfo.gameObject.tag == "Pasto")
         {
-            movementSpeed = 0.05f;
+            movementSpeed = 0.2f;
 
 
             // Si no sirve el if del void enter usar esto;
@@ -77,7 +80,11 @@ public class PlayerMovement : MonoBehaviour
             //    movementSpeed = 0.05f;
             //}
         }
+        if (collisionInfo.gameObject.tag == "Speed")
+        {
+            movementSpeed = 1f;
 
 
+        }
     }
 }
